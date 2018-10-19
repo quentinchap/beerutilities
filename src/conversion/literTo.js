849c3125a1e093ed71130d540fab8e80c3b43a42
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2018-10-20 00:51:23 +0200

    add liter conversion

---
+++ src/conversion/literTo.js
@@ -0,0 +1,13 @@
+/* Liter to X */
+export function literToOz(l) {
+    return l * 33.814;
+  }
+  
+  export function literToGal(l) {
+    return l * 0.26417;
+  }
+  
+  export function literToCl(l) {
+    return l * 100;
+  }
+  
--- src/conversion/ozTo.js
+++ src/conversion/ozTo.js
@@ -6,16 +6,3 @@ export function ozToLiter(oz) {
 export function ozToGal(oz) {
   return oz / 128;
 }
-
-/* Liter to X */
-export function literToOz(l) {
-  return l * 33.814;
-}
-
-export function literToGal(l) {
-  return l * 0.26417;
-}
-
-export function literToCl(l) {
-  return l * 100;
-}
