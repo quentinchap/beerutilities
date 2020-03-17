8e91c77e6df320a66b738641005090a4cbeec99d
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2020-03-17 21:39:21 +0100

    ref

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {literToOz, literToGal, literToCl} from './conversion/literTo';
 import {ozToGal, ozToLiter} from './conversion/ozTo';
 import {getSugarToBottling} from './conditionningTools/bottling';
 import {beerTypeCarbo} from './referential/beerTypeCarbo';
+import fermentables from './referential/fermentables';
 import {BCJP2015} from './referential/BCJP2015';
 
 export {
@@ -19,4 +20,5 @@ export {
   ozToLiter,
   beerTypeCarbo,
   getTheoricalDenity,
+  fermentables,
 };
